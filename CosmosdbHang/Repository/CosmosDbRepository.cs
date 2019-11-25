9971df0eb9c8676d2940f20eca730bb134ee8b0b
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2019-11-25 21:55:17 +0100

    Removed query stuff

--- CosmosdbHang/Repository/CosmosDbRepository.cs
+++ CosmosdbHang/Repository/CosmosDbRepository.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
-using Microsoft.Azure.Documents.Linq;
 
 namespace CosmosdbHang.Repository
 {
@@ -13,7 +11,6 @@ namespace CosmosdbHang.Repository
     {
         private readonly AsyncLazy<DocumentClient> _clientFactory;
         private readonly CosmosDBSettings _settings;
-        private Uri _collectionUri;
 
         internal CosmosDbRepository(CosmosDBSettings settings)
         {
@@ -33,7 +30,6 @@ namespace CosmosdbHang.Repository
                 var client = new DocumentClient(new Uri(_settings.Endpoint), _settings.Key, policy);
 
                 await PrepareDatabase(client).ConfigureAwait(false);
-                _collectionUri = PrepareCollection(client);
 
                 return client;
             });
@@ -58,36 +54,18 @@ namespace CosmosdbHang.Repository
             }
         }
 
-        private Uri PrepareCollection(DocumentClient client)
-        {
-            var collection = client.CreateDocumentCollectionQuery(UriFactory.CreateDatabaseUri(_settings.ContainerId)).Where(c => c.Id == _settings.ContainerId).ToArray().FirstOrDefault();
-            if (collection is null)
-            {
-                Trace.WriteLine($"Collection {_settings.ContainerId} does not exist!");
-                throw new Exception($"Collection {_settings.ContainerId} does not exist");
-            }
-
-            return UriFactory.CreateDocumentCollectionUri(_settings.DatabaseId, _settings.ContainerId);
-        }
-
-        internal async Task<ClientEntity> GetClient(Guid clientId)
+        internal async Task<bool> GetDb()
         {
             try
             {
-                var dbClient = await _clientFactory;
-                using (var query = dbClient.CreateDocumentQuery<ClientEntity>(_collectionUri, new FeedOptions { PartitionKey = new PartitionKey(clientId.ToString()) })
-                    .Where(x => x.ClientId == clientId)
-                    .AsDocumentQuery())
-                {
-
-                    var client = await query.ExecuteFirstOrDefault(nameof(GetClient)).ConfigureAwait(false);
-                    return client;
-                }
+                var client = await _clientFactory;
+                await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_settings.DatabaseId)).ConfigureAwait(false);
+                return true;
             }
-            catch (DocumentClientException ex)
+            catch (Exception e)
             {
-                Trace.WriteLine($"Unable to get the client {clientId} from storage. Exception: {ex}");
-                return null;
+                Trace.WriteLine(e);
+                throw;
             }
         }
     }
--- CosmosdbHang/Repository/DocumentQueryExtensions.cs
+++ CosmosdbHang/Repository/DocumentQueryExtensions.cs
@@ -1,8 +1,5 @@
 ﻿using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Azure.Documents.Client;
-using Microsoft.Azure.Documents.Linq;
 
 namespace CosmosdbHang.Repository
 {
@@ -11,15 +8,6 @@ namespace CosmosdbHang.Repository
         internal static void Log<T>(this ResourceResponse<T> response, string operationName) where T : Microsoft.Azure.Documents.Resource, new()
         {
             Trace.WriteLine($"{operationName} finished. Request charge is {response.RequestCharge}.");
-        }
-
-        internal static async Task<TQueryItem> ExecuteFirstOrDefault<TQueryItem>(this IDocumentQuery<TQueryItem> documentQuery, string operationName)
-        {
-            Trace.WriteLine($"Executing {operationName}");
-            var feedResponse = await documentQuery.ExecuteNextAsync<TQueryItem>().ConfigureAwait(false);
-            Trace.WriteLine($"{operationName} finished. Request charge is {feedResponse.RequestCharge}.");
-
-            return feedResponse.FirstOrDefault();
         }
     }
 }
